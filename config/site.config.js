45f121ddbf248e12c400811d8128ec200ae48765
Riko Shen <luoli.amamiya@gmail.com>
2024-09-16 15:48:55 +0800

    Adding another masked folder.

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/📚 EdD Program', '/💾 Group Meeting'],
+  protectedRoutes: ['/📚 EdD Program', '/💾 Group Meeting', '📈 Descriptive and Inferential Statistics'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: "",
